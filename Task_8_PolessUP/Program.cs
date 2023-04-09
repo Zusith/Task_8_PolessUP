33cef878ccf5bc3bbb44588b6f58e66b56614292
Kartech <kartech222@outlook.com>
2023-04-09 22:47:48 +0300

    Добавьте файлы проекта.

---
+++ Task_8_PolessUP/Program.cs
@@ -0,0 +1,27 @@
+﻿int[] rgbnums = {123, -255, 9000 };
+Console.WriteLine(RGBConvertTo16x(rgbnums));
+
+string RGBConvertTo16x(int[] rgb)
+{
+    string result = "";
+    for (int index = 0; index < rgb.Length; index++)
+    {
+        if (rgb[index] < 0)
+        {
+            rgb[index] = 0;
+        }
+        if (rgb[index] > 255)
+        {
+            rgb[index] = 255;
+        }
+        string temp = Convert.ToString(rgb[index], 16);
+        if (temp.Length < 2)
+        {
+            result += 0;
+        }
+        result += temp;
+    }
+    return result;
+}
+
+
